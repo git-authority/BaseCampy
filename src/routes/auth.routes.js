5e472b83958758320a9bc8d14dbae797d485e066
Abhrasnata Ray <abhrasnataray@gmail.com>
2026-04-21 19:54:12 +0530

    Created Secured and Unsecured Routes

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,28 @@
+import {Router} from "express";
+import {registerUser, login, logoutUser, verifyEmail, refreshRefreshToken, forgotPasswordRequest, resetForgotPassword, getCurrentUser, changeCurrentPassword, resendEmailVerification} from "../controllers/auth.controller.js"
+import { validate } from "../middlewares/validator.middleware.js";
+import { userChangeCurrentPasswordValidator, userForgotPasswordValidator, userLoginValidator, userRegisterValidator, userResetForgotPasswordValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router();
+
+// unsecured routes
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshRefreshToken);
+router.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(), validate, resetForgotPassword)
+
+
+// secure routes
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification);
+router.route("/change-password").post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword);
+router.route("/logout").post(verifyJWT, logoutUser);
+
+
+
+
+export default router;
