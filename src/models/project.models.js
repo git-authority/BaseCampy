69116b3419719557195be17575f74a25f4936f23
Abhrasnata Ray <abhrasnataray@gmail.com>
2026-04-22 13:30:15 +0530

    Created schemas for project, projectMember, Task, Subtask, Note

---
+++ src/models/project.models.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, { timestamps: true })
+
+export const Project = mongoose.model("Project", projectSchema)
